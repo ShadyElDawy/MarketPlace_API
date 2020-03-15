42f8e33396cf05b5e97140d4020c5868bf6ce435
shady <shadyeldawy96@gmail.com>
2020-03-15 13:19:24 +0200

    routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+
+		scope module: :v1 do
+
+			           constraints: ApiConstraints.new(version: 1, default: true) do
+
+
+    					end
+		end
+  end
 end
